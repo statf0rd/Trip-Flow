9677f4b5ca3d35eb2442818fb11322985a3524a2
stanislav <stas.rushkevich@gmail.com>
2025-12-27 19:21:59 +0300

    Добавить сетевой модуль Retrofit

---
+++ app/src/main/java/com/triloo/di/NetworkModule.kt
@@ -0,0 +1,91 @@
+package com.triloo.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.triloo.data.remote.CurrencyApi
+import com.triloo.data.remote.DirectionsApi
+import com.triloo.data.remote.PlacesApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder()
+            .setLenient()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+        return OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .addInterceptor(logger)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @Named("MapsRetrofit")
+    fun provideMapsRetrofit(
+        gson: Gson,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://maps.googleapis.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @Named("CurrencyRetrofit")
+    fun provideCurrencyRetrofit(
+        gson: Gson,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://open.er-api.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePlacesApi(@Named("MapsRetrofit") retrofit: Retrofit): PlacesApi {
+        return retrofit.create(PlacesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDirectionsApi(@Named("MapsRetrofit") retrofit: Retrofit): DirectionsApi {
+        return retrofit.create(DirectionsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCurrencyApi(@Named("CurrencyRetrofit") retrofit: Retrofit): CurrencyApi {
+        return retrofit.create(CurrencyApi::class.java)
+    }
+}
